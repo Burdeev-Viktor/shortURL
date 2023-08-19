b8c5a0b809498aa57ff537e0470658ab9f3398d2
Viktor <burdeevviktor01@gmail.com>
2023-08-19 20:25:25 +0300

    add refreshing jwt

--- src/main/java/org/example/controllers/rest/AuthController.java
+++ src/main/java/org/example/controllers/rest/AuthController.java
@@ -7,6 +7,7 @@ import org.example.service.AuthService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,4 +24,9 @@ public class AuthController {
     public ResponseEntity<?> createNewUser(@RequestBody RegistrationUserDto registrationUserDto) {
         return authService.createNewUser(registrationUserDto);
     }
+    @PostMapping("/refresh-token")
+    public ResponseEntity<?> refreshToken(@RequestHeader String authorization) {
+        return authService.refreshToken(authorization);
+    }
+
 }
--- src/main/java/org/example/jwt/JwtTokenUtils.java
+++ src/main/java/org/example/jwt/JwtTokenUtils.java
@@ -1,8 +1,10 @@
 package org.example.jwt;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.example.service.UserService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
@@ -13,17 +15,24 @@ import java.util.Map;
 
 @Component
 public class JwtTokenUtils {
+    private UserService userService;
     @Value("${jwt.secret}")
     private String secret;
 
     @Value("${jwt.lifetime}")
-    private long jwtLifetime;
+    private long jwtLifeTime;
+    @Value("${jwt.refreshtime}")
+    private long jwtRefreshTime;
+
+    public JwtTokenUtils(UserService userService) {
+        this.userService = userService;
+    }
 
     public String generateToken(UserDetails userDetails) {
         Map<String, Object> claims = new HashMap<>();
         claims.put("username", userDetails.getUsername());
         Date issuedDate = new Date();
-        Date expiredDate = new Date(issuedDate.getTime() + jwtLifetime);
+        Date expiredDate = new Date(issuedDate.getTime() + jwtLifeTime);
         return Jwts.builder()
                 .setClaims(claims)
                 .setSubject(userDetails.getUsername())
@@ -44,5 +53,38 @@ public class JwtTokenUtils {
     }
     public String getUsernameWhitsBearer(String token){
         return this.getUsername(token.substring(7));
+    }
+
+    public boolean isValidToken(String authorization) {
+        String username = null;
+        String jwt = null;
+        if (authorization.startsWith("Bearer_")) {
+            jwt = authorization.substring(7);
+            try {
+                username = getUsername(jwt);
+            } catch (ExpiredJwtException e) {
+                return true;
+            }catch (Exception e){
+                return false;
+            }
+            return true;
+        }
+        return false;
+    }
+    public String refreshToken(String authorization){
+        String token = authorization.substring(7);
+        long issuedTime;
+        String username = null;
+        try {
+            issuedTime = getAllClaimsFromToken(token).getIssuedAt().getTime();
+            username = getUsername(token);
+        }catch (ExpiredJwtException e){
+           issuedTime =  e.getClaims().getIssuedAt().getTime();
+           username = e.getClaims().getSubject();
+        }
+        if((new Date().getTime() - issuedTime) > jwtRefreshTime){
+            return null;
+        }
+        return generateToken(userService.loadUserByUsername(username));
     }
 }
